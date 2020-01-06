0fc1b2e06bb06fc59ddcd08065876853810bba64
Stephen von Takach <steve@acaprojects.com>
2020-01-07 08:18:42 +1100

    fix: only set rejection cause if the errors are different

--- src/promise/deferred_promise.cr
+++ src/promise/deferred_promise.cr
@@ -1,3 +1,5 @@
+require "./core_ext"
+
 class Promise::DeferredPromise(Input) < Promise
   def initialize
     @defer = Deferred(Input).new(self)
@@ -115,7 +117,7 @@ class Promise::DeferredPromise(Input) < Promise
         ret = errback.call(reason)
         ret.is_a?(Exception) ? result.reject(ret) : result.resolve(ret)
       rescue error
-        error.cause = reason
+        error.cause = reason unless error == reason
         result.reject(error)
       end
       reason
@@ -184,23 +186,4 @@ class Promise::DeferredPromise(Input) < Promise
 
     res
   end
-end
-
-class Object
-  macro __check_if_promise__
-    {% if @type.ancestors.includes?(::Promise) %}
-      ret_actual = self.type_var
-      -> { ret_actual }
-    {% else %}
-      -> { self }
-    {% end %}
-  end
-
-  def __check_for_promise__
-    __check_if_promise__
-  end
-end
-
-class Exception
-  property cause : Exception?
 end
